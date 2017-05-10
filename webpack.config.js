a0a210eb0750e5b8db6546e9835db94360456234
Andy Anderson <andyanderson1818@gmail.com>
2017-05-09 17:02:38 -0700

    Installing webpack

---
+++ webpack.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  entry: "./app/assets/scripts/App.js",
+  output: {
+    path: "/Users/NigelFarageMEP/CODE/Tutorials/WDEssentials/travelsitecopy/app/temp/scripts",
+    filename: "App.js"
+  }
+}
