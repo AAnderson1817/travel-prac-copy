bcc4299d2c876a675e43327e91e35ba60577193f
Andy Anderson <andyanderson1818@gmail.com>
2017-05-09 17:39:25 -0700

    Added babel

--- webpack.config.js
+++ webpack.config.js
@@ -3,5 +3,17 @@ module.exports = {
   output: {
     path: "/Users/NigelFarageMEP/CODE/Tutorials/WDEssentials/travelsitecopy/app/temp/scripts",
     filename: "App.js"
+  },
+  module: {
+    loaders: [
+      {
+        loader: 'babel',
+        query: {
+          presets: ['es2015']
+        },
+        test: /\.js$/,
+        exclude: /node_modules/
+      }
+    ]
   }
 }
