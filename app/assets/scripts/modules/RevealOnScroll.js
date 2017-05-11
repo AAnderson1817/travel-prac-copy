704ad60d8dfa1ec733e30dc32851f82af164a8c8
Andy Anderson <andyanderson1818@gmail.com>
2017-05-11 11:33:16 -0700

    Beginning sc on-scroll

---
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -0,0 +1,19 @@
+import $ from 'jquery';
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
+class RevealOnScroll {
+  constructor() {
+    this.itemsToReveal = $(".feature-item");
+    this.hideInitially();
+  }
+
+  hideInitially(){
+    this.itemsToReveal.addClass("reveal-item");
+  }
+
+  createWaypoints(){
+    
+  }
+}
+
+export default RevealOnScroll
