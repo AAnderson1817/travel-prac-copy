0c73cdf2131f96b0b851f3db9bbd73cc57486a86
Andy Anderson <andyanderson1818@gmail.com>
2017-05-12 13:21:58 -0700

    Fixing old versions of modules

--- gulp/tasks/sprites.js
+++ gulp/tasks/sprites.js
@@ -5,7 +5,6 @@ del = require('del'),
 svg2png = require('gulp-svg2png');
 
 var config = {
-  //We use "shape, spacing, and padding properties" to add a bit of spacing between the sprites. This helps prevent artifacts from neighboring sprites from appearing. //
   shape: {
     spacing: {
       padding: 1
@@ -14,7 +13,6 @@ var config = {
   mode: {
     css: {
       variables: {
-        //This function takes the file name string i.e. "fox.svg" and splits the name from it's file type designation; "fox" and ".svg". The function then joins together "fox" with ".png" to create "fox.png" which we need to make our site as browser-friendly as possible. //
         replaceSvgWithPng: function() {
           return function(sprite, render) {
             return render(sprite).split('.svg').join('.png');
@@ -34,7 +32,7 @@ var config = {
 gulp.task('beginClean', function() {
   return del(['./app/temp/sprite', './app/assets/images/sprites']);
 });
-//Items in square brackets are known as "dependencies". Simply, if a function has a dependency, the dependency must complete first before the function will run. 'createSprite', for example, waits for 'beginClean' to complete before firing.//
+
 gulp.task('createSprite', ['beginClean'], function() {
   return gulp.src('./app/assets/images/icons/**/*.svg')
     .pipe(svgSprite(config))
