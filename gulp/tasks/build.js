c7974c708ccc87a00811510dd7a0c8e81875feb9
Andy Anderson <andyanderson1818@gmail.com>
2017-05-12 13:10:50 -0700

    Added in Finished adding in previewDist

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,14 +1,42 @@
 var gulp = require('gulp'),
 imagemin = require('gulp-imagemin'),
 del = require('del'),
-usemin = require('gulp-usemin');
+usemin = require('gulp-usemin'),
+rev = require('gulp-rev'),
+cssnano = require('gulp-cssnano'),
+uglify = require('gulp-uglify'),
+browserSync = require('browser-sync').create();
+
+gulp.task('previewDist', function(){
+  browserSync.init({
+    notify: false,
+    server: {
+      baseDir: "dist"
+    }
+  });
+})
 
 gulp.task('deleteDistFolder', function(){
   return del("./dist");
 })
 
+gulp.task('copyGeneralFiles',['deleteDistFolder'], function(){
+  var pathsToCopy = [
+      './app/**/*',
+      '!./app/index.html',
+      '!./app/assets/imagse/**',
+      '!./app/assets/styles/**',
+      '!./app/assets/scripts/**',
+      '!./app/temp',
+      '!./app/temp/**'
+  ];
 
-gulp.task('optimizeImages',['deleteDistFolder'], function(){
+  return gulp.src(pathsToCopy)
+    .pipe(gulp.dest('./dist'));
+});
+
+
+gulp.task('optimizeImages',['deleteDistFolder','icons'], function(){
   //The code below takes all the images in our development folder and excludes any icons and related files. Users only need the sprites. We'll pipe it through a compressor module and send it to the /dist folder for distribution. //
   return gulp.src(['./app/assets/images/**/*.', '!app/assets/images/icons', '!./app/assets/images/icons/**/*'])
     .pipe(imagemin({
@@ -19,10 +47,13 @@ gulp.task('optimizeImages',['deleteDistFolder'], function(){
     .pipe(gulp.dest("./dist/assets/images"));
 });
 
-gulp.task('usemin',['deleteDistFolder'], function(){
+gulp.task('usemin',['deleteDistFolder','styles', 'scripts'], function(){
   return gulp.src("./app/index.html")
-    .pipe(usemin())
+    .pipe(usemin({
+      css: [function(){return rev()}, function(){return cssnano()}],
+      js: [function(){return rev()}, function(){return uglify()}]
+    }))
     .pipe(gulp.dest("./dist"));
 })
 
-gulp.task('build', ['deleteDistFolder','optimizeImages', 'usemin']);
+gulp.task('build', ['deleteDistFolder','copyGeneralFiles','optimizeImages', 'usemin']);
