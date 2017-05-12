52a680167db8219241d372377ab8111493fa5328
Andy Anderson <andyanderson1818@gmail.com>
2017-05-12 12:47:19 -0700

    Added usemin and

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,7 +1,14 @@
 var gulp = require('gulp'),
-imagemin = require('gulp-imagemin');
+imagemin = require('gulp-imagemin'),
+del = require('del'),
+usemin = require('gulp-usemin');
 
-gulp.task('optimizeImages', function(){
+gulp.task('deleteDistFolder', function(){
+  return del("./dist");
+})
+
+
+gulp.task('optimizeImages',['deleteDistFolder'], function(){
   //The code below takes all the images in our development folder and excludes any icons and related files. Users only need the sprites. We'll pipe it through a compressor module and send it to the /dist folder for distribution. //
   return gulp.src(['./app/assets/images/**/*.', '!app/assets/images/icons', '!./app/assets/images/icons/**/*'])
     .pipe(imagemin({
@@ -12,4 +19,10 @@ gulp.task('optimizeImages', function(){
     .pipe(gulp.dest("./dist/assets/images"));
 });
 
-gulp.task('build', ['optimizeImages'])
+gulp.task('usemin',['deleteDistFolder'], function(){
+  return gulp.src("./app/index.html")
+    .pipe(usemin())
+    .pipe(gulp.dest("./dist"));
+})
+
+gulp.task('build', ['deleteDistFolder','optimizeImages', 'usemin']);
